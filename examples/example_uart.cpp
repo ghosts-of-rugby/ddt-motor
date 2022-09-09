0b89e33a7ed75b7afc525f4f6e76af4e4da1105a
Kenji Fukuda <fk.kenji.21@gmail.com>
2022-09-09 12:29:43 +0900

    add receive command in example

--- examples/example_uart.cpp
+++ examples/example_uart.cpp
@@ -3,7 +3,10 @@
 int main(int argc, char const *argv[]) {
   ddt::Uart uart("/dev/ttyUSB0", ddt::Uart::BaudRate::B_115200);
 
-  uart.Send({0x12});
+  uart.Send({0x12,0x60});
+
+  auto data = uart.Receive();
+   
 
   return 0;
 }
